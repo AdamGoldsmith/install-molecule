6af31916449d96734b1decd1bafe644c134eb0f3
Adam Goldsmith <adam.goldsmith75@gmail.com>
2017-11-28 11:21:19 +0000

    Remove ${PWD} from DEMO_PROMPT

--- install-molecule.sh
+++ install-molecule.sh
@@ -20,7 +20,7 @@
 #
 # see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
 #
-DEMO_PROMPT="${GREEN}\u@${NICKNAME}${BLACK}:${BLUE}\${PWD} ${BLACK}\$ "
+DEMO_PROMPT="${GREEN}\u@${NICKNAME}${BLACK}: ${BLACK}\$ "
 
 # hide the evidence
 clear
