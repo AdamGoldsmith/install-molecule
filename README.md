60ddde5e5a9a6787e3dbc6f418dad30527d96802
Molecule Administrator <molecule@molecule.dom1>
2017-11-28 11:30:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## Demo-magic script to install molecule
+
+### Requirements
+
+Git
+
+`sudo yum install git`
+
+[demo-magic](https://github.com/paxtonhare/demo-magic.git)
+
+`git clone https://github.com/paxtonhare/demo-magic.git`
+
+### Running it
+
+```
+git clone
+cd install-molecule
+./install-molecule.sh
+```
+
+
+### Tested on
+
+* Centos 7.4
+
+### Author
+
+*Adam Goldsmith*
+
---
+++ install-molecule.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+########################
+# include the magic
+########################
+. /usr/local/bin/demo-magic.sh
+
+
+########################
+# Configure the options
+########################
+
+#
+# speed at which to simulate typing. bigger num = faster
+#
+# TYPE_SPEED=20
+
+#
+# custom prompt
+#
+# see http://www.tldp.org/HOWTO/Bash-Prompt-HOWTO/bash-prompt-escape-sequences.html for escape sequences
+#
+DEMO_PROMPT="${GREEN}\u@${NICKNAME}${BLACK}:${BLUE}\${PWD} ${BLACK}\$ "
+
+# hide the evidence
+clear
+
+
+# put your demo awesomeness here
+pe "sudo yum install gcc python-devel openssl-devel libffi-devel python-pip"
+pe "sudo pip install --upgrade pip"
+pe "sudo pip install ansible"
+pe "sudo pip install docker"
+pe "sudo pip install molecule"
+
